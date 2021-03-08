ae8340ec3d9b86b58aea04ab31996cf14974269e
MonicaTriana <monica_rt@outlook.com>
2021-03-08 16:39:34 -0600

    modulacion recursos y requestHandler

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,19 +1,8 @@
 const http = require('http');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const enrutador = require('./enrutador');
 const requestHandler = require('./request-handler');
+const recursos = require('./recursos')
 
-global.recursos = {
-    mascotas: [
-            {tipo: "perro", nombre: "Andy", propietario: "Vince"},
-            {tipo: "perro", nombre: "Andy", propietario: "Vince"},
-            {tipo: "perro", nombre: "Andy", propietario: "Vince"},
-            {tipo: "perro", nombre: "Andy", propietario: "Vince"},
-            {tipo: "perro", nombre: "Andy", propietario: "Vince"},
-    ],
-};
-
+global.recursos = recursos;
 const server = http.createServer(requestHandler); 
 server.listen(5000, () => {
     console.log('El servidor esta eschuchando peticiones en http://localhost:5000');
---
+++ nodejs/recursos.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mascotas: [
+            {tipo: "perro", nombre: "Andy", propietario: "Vince"},
+            {tipo: "perro", nombre: "Andy", propietario: "Vince"},
+            {tipo: "perro", nombre: "Andy", propietario: "Vince"},
+            {tipo: "perro", nombre: "Andy", propietario: "Vince"},
+            {tipo: "perro", nombre: "Andy", propietario: "Vince"},
+    ],
+};
