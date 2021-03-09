87d1805bfb7996472a01169037e8add5d724c28c
MonicaTriana <imonica_rt@outlook.com>
2021-03-09 14:42:41 -0600

    refactor rutas

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,9 +1,8 @@
 const http = require('http');
 const requestHandler = require('./request-handler');
-const recursos = require('./recursos')
-
-global.recursos = recursos;
 const server = http.createServer(requestHandler); 
+
 server.listen(5000, () => {
-    console.log('El servidor esta eschuchando peticiones en http://localhost:5000');
+    console.log('El servidor esta eschuchando peticiones en http://localhost:5000'
+    );
 });
