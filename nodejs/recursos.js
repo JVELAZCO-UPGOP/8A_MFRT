921b969e027cd4c50eb1f8a5fa7b1c49fffe4c59
MonicaTriana <imonica_rt@outlook.com>
2021-03-09 20:07:45 -0600

    rutas veterinarios

--- nodejs/recursos.js
+++ nodejs/recursos.js
@@ -6,4 +6,11 @@ module.exports = {
             {tipo: "perro", nombre: "Andy3", propietario: "Vince"},
             {tipo: "perro", nombre: "Andy4", propietario: "Vince"},
     ],
+    veterinarios: [
+        {nombre: "Juanito", apellido:"Hernandez", documento:"1234567890"},
+        {nombre: "Ale", apellido:"Muñoz", documento:"1234777790"},
+        {nombre: "Fer", apellido:"Triana", documento:"1233333333"},
+        {nombre: "Vero", apellido:"Silva", documento:"00000067890"},
+        {nombre: "Vince", apellido:"Gonzalez", documento:"111111111111"},
+],
 };
