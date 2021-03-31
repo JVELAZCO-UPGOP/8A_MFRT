c376381ab8a51f19763f7de86a41577d6a0c09b5
MonicaTriana <imonica_rt@outlook.com>
2021-03-30 21:36:36 -0600

    reusar componentes de mascotas como componente principal

--- react-2/src/App.js
+++ react-2/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
-import Mascotas from './Mascotas';
+import Pagina from './Pagina';
 
 function App() {
-  return ( <Mascotas/>
+  return ( <Pagina/>
   );
 }
 
