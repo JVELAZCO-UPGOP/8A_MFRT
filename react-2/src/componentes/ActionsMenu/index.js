812907bd3ff674be152152c3d4785d4efb9186e1
MonicaTriana <imonica_rt@outlook.com>
2021-04-02 23:48:53 -0600

    Componente alert

--- react-2/src/componentes/ActionsMenu/index.js
+++ react-2/src/componentes/ActionsMenu/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ActionsMenu.css";
+import Alert from "../Alert";
 
 function ActionsMenu(){
     return (
@@ -9,12 +10,7 @@ function ActionsMenu(){
             <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
             Nueva
             </button>
-                <div className="alert alert-danger alert-dismissible" role="alert">
-                    <strong>Oops!</strong> Algo hicimos mal, por favor vuelve a intentarlo!.
-                    <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
+            <Alert/>
             </div>
         </div>
     );
