41c19c0f4f1a12fd052c3ba558af4177c55a95c2
MonicaTriana <imonica_rt@outlook.com>
2021-04-06 12:07:55 -0500

    mostrar y ocultar alert con react hook useState

--- react-2/src/componentes/ActionsMenu/index.js
+++ react-2/src/componentes/ActionsMenu/index.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ActionsMenu.css";
 import Alert from "../Alert";
 
 function ActionsMenu(){
+    const [mostrarAlerta, setMostrarAlerta] = useState(false);
     return (
         <div className="actions-menu">
             <h1>Macotas</h1>
             <div className="actions-menu-content">
-            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter" onClick={()=>setMostrarAlerta(!mostrarAlerta)}>
             Nueva
             </button>
-            <Alert/>
+            {mostrarAlerta && <Alert/> }
             </div>
         </div>
     );
