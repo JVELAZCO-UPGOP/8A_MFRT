a2a9d2561f82414038db43ea564a55539c378d9e
MonicaTriana <imonica_rt@outlook.com>
2021-04-06 12:49:10 -0500

    Pasar function como prop de un componente para cambiar el estado del padre

--- react-2/src/componentes/ActionsMenu/index.js
+++ react-2/src/componentes/ActionsMenu/index.js
@@ -4,14 +4,15 @@ import Alert from "../Alert";
 
 function ActionsMenu(){
     const [mostrarAlerta, setMostrarAlerta] = useState(false);
+    const alertSwitch = () => setMostrarAlerta(!mostrarAlerta)
     return (
         <div className="actions-menu">
             <h1>Macotas</h1>
             <div className="actions-menu-content">
-            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter" onClick={()=>setMostrarAlerta(!mostrarAlerta)}>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter" onClick={alertSwitch}>
             Nueva
             </button>
-            {mostrarAlerta && <Alert/> }
+            {mostrarAlerta && <Alert alertSwitch = {alertSwitch}/> }
             </div>
         </div>
     );
--- react-2/src/componentes/Alert/index.js
+++ react-2/src/componentes/Alert/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Alert.css";
 
-function Alert(){
+function Alert(props){
     return (
         <div className="alert alert-danger alert-dismissible" role="alert">
                 <strong>Oops!</strong> Algo hicimos mal, por favor vuelve a intentarlo!.
-                <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                <button type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={props.alertSwitch}>
                 <span aria-hidden="true">&times;</span>
                 </button>
         </div>
