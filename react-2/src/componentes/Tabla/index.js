ecac99e3a3add6f54932fb05cac5930260e51466
MonicaTriana <imonica_rt@outlook.com>
2021-04-06 16:10:57 -0500

    componente fila

--- react-2/src/componentes/Tabla/index.js
+++ react-2/src/componentes/Tabla/index.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Encabezado from "./Encabezado";
+import Fila from "./Fila";
 
 function Tabla(){
     const [mascotas, setMascotas] = useState([
@@ -17,22 +18,12 @@ function Tabla(){
     const columnas = mascotas.length > 0 ? Object.keys(mascotas[0]) : [];
     return (
         <table className="table table-striped table-hover ">
-            <tbody id="lista-mascotas" >
             <Encabezado columnas = {columnas}/>
+            <tbody id="lista-mascotas" >
             {mascotas.map(
-            (mascota, index) => <tr>
-            <th scope="row">{index}</th>
-            <td>{mascota.tipo}</td>
-            <td>{mascota.nombre}</td>
-            <td>{mascota.propietario}</td>
-            <td>
-                <div class="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" class="btn btn-info editar"><i class="fas fa-edit"></i></button>
-                    <button type="button" class="btn btn-danger eliminar"><i class="far fa-trash-alt"></i></button>
-                </div>
-            </td>
-        </tr>
-        )}
+            (mascota, index) => (
+            <Fila mascota={mascota} index={index}/>
+        ))}
         </tbody>
         </table>
     );
