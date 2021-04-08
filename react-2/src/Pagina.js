a453ea2c3ee9de5e1b54e16a3e8ce154cc7e3ce4
MonicaTriana <imonica_rt@outlook.com>
2021-04-08 15:44:50 -0500

    classcomponent y mostrar y ocultar ventana modal

--- react-2/src/Pagina.js
+++ react-2/src/Pagina.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, {Component} from "react";
 import Nav from "./componentes/Nav";
 import ActionsMenu from "./componentes/ActionsMenu";
 import Tabla from "./componentes/Tabla";
 import Modal from "./componentes/Modal";
 
-function Mascotas() {
+class Pagina extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        mostrarModal: false,
+        };
+    }
+
+    cambiarModal = () => {
+this.setState({mostrarModal: !this.state.mostrarModal})
+    };
+
+//Codigo del componente
+
+//el método render siempre debe ir de ultimo
+    render (){
     return (
-    <>
-    <div className="container">    
-        <Nav/>
-        <ActionsMenu/>
-        <Tabla/>
-        <Modal/>
-    </div>
-    </>
-    );
+        <>
+        <div className="container">    
+            <Nav/>
+            <ActionsMenu cambiarModal = {this.cambiarModal}/>
+            <Tabla/>
+            {this.state.mostrarModal && <Modal/>}
+        </div>
+        </>
+        );
+    }  
 } 
-export default Mascotas;
+export default Pagina;
