0b29377e486f8dba0c798a156ce93eea64425142
MonicaTriana <imonica_rt@outlook.com>
2021-04-10 13:34:30 -0500

    Modal footer cambiar modal

--- react-2/src/componentes/Modal/ModalFooter.js
+++ react-2/src/componentes/Modal/ModalFooter.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./ModalFooter.css";
 
-function ModalFooter(){
+function ModalFooter({cambiarModal = () =>{} }){
     return(
     <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-        <button type="button" className="btn btn-primary" data-dismiss="modal" id="btn-guardar">Crear</button>
+        <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={cambiarModal}>Cerrar</button>
+        <button type="button" className="btn btn-primary" data-dismiss="modal" id="btn-guardar" onClick={cambiarModal}>Crear</button>
     </div>
     );
 }
