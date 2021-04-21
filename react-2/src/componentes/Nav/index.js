dd0c9c16ea338f3f8df57fc2983f1a253c615048
MonicaTriana <imonica_rt@outlook.com>
2021-04-20 23:09:48 -0500

    Mejorando estilos del componente Search

--- react-2/src/componentes/Nav/index.js
+++ react-2/src/componentes/Nav/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./Nav.css";
+import Search from "../Search";
 
 function Nav() {
     return ( 
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <a className="navbar-brand" href="#">Veterinaria</a>
-                {/*<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>*/}
-            
-            <div className="collapse navbar-collapse" id="navbarColor01">
+            <div className="navbar-right" id="navbarColor01">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
                         <a className="nav-link" href="/index.html">Mascotas<span class="sr-only">(current)</span></a>
@@ -24,10 +21,7 @@ function Nav() {
                         <a className="nav-link" href="/Dueños.html">Dueños</a>
                     </li>
                 </ul>
-                <form className="form-inline">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                    <button className="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
-                </form>
+                <Search/>
             </div>
         </nav>
     );
